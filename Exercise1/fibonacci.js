e9fe548d6860358d64c7984abbfb95eb65b8c08c
Quartezz <kamil.machel03@gmail.com>
2023-09-11 15:56:03 +0200

    created directory for fibonacci exercises

---
+++ Exercise1/fibonacci.js
@@ -0,0 +1,15 @@
+function fib(n) {
+    if (n <= 1) {
+        return n;
+    }
+
+    let fib = [0, 1];
+    for (let i = 2; i < n; i++) {
+        fib[i] = fib[i - 1] + fib[i - 2];
+    }
+
+    return fib;
+}
+
+console.log(fib(8));
+
